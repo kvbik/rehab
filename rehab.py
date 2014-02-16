ec12ca8db0686d6a70fa830582f8c2ad3ca915f3
Jakub Vysoky <jakub@borka.cz>
2014-02-16 21:52:43 +0100

    simple layout of the tool

---
+++ rehab.py
@@ -0,0 +1,52 @@
+import sys
+
+class Repository(object):
+    @classmethod
+    def by_tag(cls, tag, name, *args):
+        "create repository of proper class given by tag"
+        classes = {
+            'git': Git,
+        }
+        C = classes.get(tag, cls)
+        return C(name, *args)
+
+    @classmethod
+    def loop(cls, config, options):
+        "iterate through repositories from config"
+        for i in config.data['repositories']:
+            tag, name = i[:2]
+            args = i[2:]
+            yield cls.by_tag(tag, name, *args)
+
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+
+class Git(Repository):
+    "git vcs abstraction"
+
+    def __init__(self, name, branch):
+        self.name = name
+        self.branch = branch
+
+class Configuration(object):
+    "configuration wrapper"
+
+    def __init__(self, config, data=None):
+        self.config = config
+        self.data = data or {}
+
+    @classmethod
+    def parse(cls, argv):
+        "parse command line options and load configuration"
+        config = cls()
+        options = {}
+        return config, options
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    config, options = Configuration.parse(argv)
+
+if __name__ == '__main__':
+    main()
+
---
+++ test_rehab.py
@@ -0,0 +1,49 @@
+from nose import tools
+
+from rehab import Git, Repository
+from rehab import Configuration
+
+# basic rehab configuration, default values will be pretty similar
+CONFIG = {
+    # where your repositories will checkout to
+    'repodir': '/var/repos/',
+
+    # list of repositories: type, name and other params
+    'repositories': [
+        ('git', 'git@github.com:kvbik/python-baf.git', 'master'),
+    ],
+
+    # run commands for each file which has changed since the last run
+    'updatehooks': {
+        'git@github.com:kvbik/python-baf.git': [
+            ('requirements.txt', 'pip install -r requirements.txt'),
+            ('requirements.txt', 'python setup.py develop'),
+            ('setup.py', 'python setup.py develop'),
+        ],
+    },
+}
+
+def test_repo_object_and_its_properties():
+    tools.assert_equals('name', Repository('name').name)
+
+def test_repo_create_by_tag_proper_object():
+    repo = Repository.by_tag('whatever', 'name')
+    tools.assert_is_instance(repo, Repository)
+
+def test_repo_loop_iterates_over_repositories_from_config():
+    conf = Configuration('conf', CONFIG)
+    repo = list(Repository.loop(conf, {}))[0]
+    tools.assert_is_instance(repo, Git)
+    tools.assert_equals('git@github.com:kvbik/python-baf.git', repo.name)
+    tools.assert_equals('master', repo.branch)
+
+def test_config_object_and_its_properties():
+    conf = Configuration('blah')
+    tools.assert_equals('blah', conf.config)
+    tools.assert_equals({}, conf.data)
+
+def test_config_with_data_contains_data():
+    conf = Configuration('with-data', CONFIG)
+    tools.assert_equals('with-data', conf.config)
+    tools.assert_equals(CONFIG, conf.data)
+
